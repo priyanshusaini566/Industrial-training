9a63b31caf29b5b96f3e8448a8737d5dbfedb2d6
priyanshu saini <ps56646i4@gmail.com>
2026-06-04 22:24:52 +0530

    Updated string.py

--- day-3/string.py
+++ day-3/string.py
@@ -1,9 +1,25 @@
 '''
 this is a string
 '''
+
+"""
 a="hello"
 b="world"
 print(a+b)
 print(a*2)
 s='hello world this is india'
 print(s)
+"""
+
+"""
+not allowed
+s="happy birthday"
+s[0]="p"
+print(s)
+"""
+
+#allowed in lists
+t=[1,"july",5,"june"]
+t[1]="september"
+print(t)
+
