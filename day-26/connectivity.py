4d46aa22e2b22b4f9cfd42b9f4ef31d1b352bec0
priyanshu saini <ps56646i4@gmail.com>
2026-06-30 23:39:20 +0530

    ./

---
+++ day-26/connectivity.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+# Database se connect karna
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="9024305576",
+    database="project_sql_latest"
+)
+
+cursor = conn.cursor()
+# Aapki likhi hui query run karna
+cursor.execute("SELECT ROUND(SUM(sales), 2) FROM fact_sales")
+total_sales = cursor.fetchone()
+
+print(f"Hey Manager! Hamari total sales hai: {total_sales[0]}")
+conn.close()
